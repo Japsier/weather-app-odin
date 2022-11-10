899baa50e4b5423a232e9704aa48622147dac33c
JapSir <japsircode@gmail.com>
2022-11-10 19:59:38 +0100

    add weather info to webpage

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import './style.css';
 
 let inputbox = document.querySelector("#search")
 let submitButton = document.querySelector(".searchButton")
+let degreeSwitch = document.querySelector("#degreeSwitch")
 
 submitButton.addEventListener("click", () => {
     let country = inputbox.value
@@ -17,14 +18,12 @@ async function getData (country) {
         let lat;
         let lon;
         for(let i = 0; i < jsonFile.length; i++) {
-            if(jsonFile[i].name.toUpperCase() == country.toUpperCase()) {
+            if(jsonFile[i].name.toUpperCase() === country.toUpperCase()) {
                 lat = jsonFile[i].lat;
                 lon = jsonFile[i].lon;
-            } //else if (jsonFile[i].name.stringsplit(" ").join("") == country.stringsplit(" ").join("")) {
-                //lat = jsonFile[i].lat;
-                //lon = jsonFile[i].lon;
-
-            //}
+                document.querySelector("#city").innerText = jsonFile[i].name
+                i = jsonFile.length
+            } 
         }
         console.log(lat)
         console.log(lon)
@@ -38,12 +37,24 @@ async function getWeather(lat, lon) {
     let data = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=9da5f0d77aac89c44adc161546b98485`, {mode: "cors"})
     let dataJson = await data.json()
     console.log(dataJson)
-    let degree = dataJson.main.temp
+    let degrees = dataJson.main.temp
+    if (degreeSwitch.checked === true) {
+        document.getElementById("degrees").innerText  = "temperature: " + Math.round((1.8*(degrees-273.15) + 32) *100)/100 + " °F"
+    } else {
+        document.getElementById("degrees").innerText = "temperature: " +  Math.round((degrees - 272.15) * 100)/100 + " °C"
+    }
     let tempFeelsLike = dataJson.main.feels_like
+    if (degreeSwitch.checked === true) {
+        document.getElementById("feels-like").innerText  = "Temperature feels like: " + Math.round((1.8*(tempFeelsLike-273.15) + 32) *100)/100 + " °F"
+    } else {
+        document.getElementById("feels-like").innerText = "Temperature feels like: " + Math.round((tempFeelsLike - 272.15) * 100)/100 + " °C"
+    }
     let weatherDescription = dataJson.weather[0].description
+    document.getElementById("description").innerText = weatherDescription
     let humidity = dataJson.main.humidity
+    document.getElementById("humidity").innerText = "humidity: " + humidity
     console.log(dataJson.weather[0].description)
 }
-let domStuff = () => {
-    
-}
+
+getData("Amsterdam")
+inputbox.value = "Amsterdam"
